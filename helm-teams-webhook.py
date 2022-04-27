f6def5db781b0556d108a61a39be1ebcf504d141
scenox <scenox@users.noreply.github.com>
2022-04-27 14:22:54 +0200

    Create helm-teams-webhook.py

---
+++ helm-teams-webhook.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+import json
+import requests
+import yaml # pip install pyyaml
+
+repo='https://<some-helm-chart-repo>/index.yaml'
+teams_webhook_url='https://somecompany.webhook.office.com/...'
+response=requests.get(repo,auth=('<username>','<pass>'))
+now = datetime.utcnow()
+index=response.text
+idxdict=yaml.safe_load(index)
+apps=list(idxdict['entries'].keys())
+for app in apps:
+    created = idxdict['entries'][app][0]['created']
+    created_iso = datetime.fromisoformat(created[:created.find('.')+7])
+    if now - created_iso < timedelta(minutes=10):
+    #if True:
+        print('New release!!')
+        version = idxdict['entries'][app][0]['version']
+        jsondata = {'@type': 'MessageCard', '@context': 'http://schema.org/extensions', 'themeColor': '0076D7', 'summary': 'New release!', 'sections': [{'activityTitle': 'New release available! ' + app, 'activitySubtitle': version, 'activityImage': 'https://helm.sh/img/helm.svg', 'facts': [{'name': 'Software', 'value': app}, {'name': 'Version', 'value': version}, {'name': 'Released', 'value': created_iso.isoformat()+'Z'}], 'markdown': True}]}
+        # send webhook with json payload
+        requests.post(teams_webhook_url, json=jsondata)
+        #print(json.dumps(jsondata))
